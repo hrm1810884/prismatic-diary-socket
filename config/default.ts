730014ad790e6bf70ce58c047a6f5dc56aed8353
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-06-26 01:54:37 +0900

    reset to local

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,17 @@
 export default {
-  corsOrigin: ["http://localhost:3000", "http://localhost:3001"],
+  corsOrigin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
+    "http://localhost:3004",
+    // "http://192.168.0.105:3000",
+    // "http://192.168.0.105:3001",
+    // "http://192.168.0.105:3002",
+    // "http://192.168.0.105:3003",
+    // "http://192.168.0.105:3004",
+  ],
   port: 8081,
   host: "localhost",
+  // host: "192.168.0.105",
 };
